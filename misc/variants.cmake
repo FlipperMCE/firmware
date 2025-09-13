173365e042252273767ccc29831035f025882787
BBsan <bbsan2k@gmail.com>
2025-09-13 16:45:40 +0200

    Update Variants

--- misc/variants.cmake
+++ misc/variants.cmake
@@ -26,33 +26,7 @@ if (VARIANT STREQUAL "FlipperMCE")
                             "PIN_BTN_LEFT=21"
                             "PIN_BTN_RIGHT=23"
                             )
-    set(FLIPPERMCE_WITH_GUI TRUE)
-    set(FLIPPERMCE TRUE)
-    add_compile_definitions(PICO_FLASH_SIZE_BYTES=16777216)
-elseif (VARIANT STREQUAL "FlipperMCE_bf_proto")
-    set(PIN_GC_INT 16)
-    set(PIN_GC_SEL 17)
-    set(PIN_GC_CLK 18)
-    set(PIN_GC_DI 19)
-    set(PIN_GC_DO 20)
-    add_compile_definitions("UART_TX=8"
-                            "UART_RX=9"
-                            "UART_PERIPH=uart1"
-                            "UART_BAUD=3000000"
-                            "SD_PERIPH=SPI1"
-                            "SD_MISO=24"
-                            "SD_MOSI=27"
-                            "SD_SCK=26"
-                            "SD_CS=29"
-                            "FLASH_OFF_EEPROM=0x7fc000"
-                            "MMCE_PRODUCT_ID=0x1"
-                            "PIN_SENSE=15"
-                            "PIN_BTN_LEFT=21"
-                            "PIN_BTN_RIGHT=23"
-                            )
-    set(FLIPPERMCE_WITH_GUI TRUE)
-    set(FLIPPERMCE TRUE)
-    add_compile_definitions(PICO_FLASH_SIZE_BYTES=16777216)
+    add_compile_definitions(PICO_FLASH_SIZE_BYTES=2097152)
 else()
     set(PIN_GC_INT 16)
     set(PIN_GC_SEL 17)
